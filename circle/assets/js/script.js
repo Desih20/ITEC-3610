799ed396fefda0a203c77b0a8c9b7c78560a4048
Desih20 <113609333+Desih20@users.noreply.github.com>
2024-02-10 04:31:23 +0000

    Updated circle

---
+++ circle/assets/js/script.js
@@ -0,0 +1,23 @@
+$("#CircleForm").validate();
+    function displayCircle() {
+
+            if ($("#CircleForm") .validate()){     
+                var radius = parseFloat(document.getElementById("radius").value);
+
+                var diameter = 2 * radius;
+                var circumference = 2 * Math.PI * radius;
+                var area = Math.PI * radius * radius;
+
+                document.getElementById("diameter").innerText = diameter.toFixed(2);
+                document.getElementById("circumference").innerText = circumference.toFixed(2);
+                document.getElementById("area").innerText = area.toFixed(2);
+}
+}
+
+function clearForm(){
+    document.getElementById("radius").value="";
+    document.getElementById("radiuserror").innerText="";
+    document.getElementById("diameter").innerText="";
+    document.getElementById("circumference").innerText="";
+    document.getElementById("area").innerText="";
+}
