d299c62ec5f4376866d2cce1a60dd32e06d3bd4a
Desih20 <113609333+Desih20@users.noreply.github.com>
2024-02-29 19:42:45 +0000

    Updated photography

--- photography/assets/js/script.js
+++ photography/assets/js/script.js
@@ -1,6 +1,6 @@
-function show{
-  if(document.getElementsByClass('content').style.display=='none')
-  document.getElementsByClass('content').style.display=='block'
-else
-document.getElementsByClass('content').style.display=='none'
-}
+$(document).ready(function() {
+  $('element').slick({
+    slidesToShow:4,
+    slidesToScroll:4
+  });
+});
