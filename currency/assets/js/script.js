f924a8d3cd5cb5ae123e9ba2d77e18c5535960c8
Desih20 <113609333+Desih20@users.noreply.github.com>
2024-03-19 00:01:01 +0000

    Updated currency and added currency

---
+++ currency/assets/js/script.js
@@ -0,0 +1,90 @@
+async function getResults() {
+    "use strict";
+     // Get a reference to the form - Use the ID of the form
+    let form = $("#myform");
+     // Validate all of the form elements
+    form.validate();
+     // If all of the form elements are valid, the get the form values
+    if (form.valid()) {
+      let baseCurrency = document.getElementById("baseCurrency").value;
+      let toCurrency = document.getElementById("toCurrency").value;
+      let apiKey = "951jAjDkFUGivM5hi8EzX2uMnLEPaf2L";
+      let FromDate = document.getElementById("FromDate").value;
+      let ToDate = document.getElementById("ToDate").value;
+       /* URL for AJAX Call */
+      let myURL =
+        "https://api.polygon.io/v2/aggs/ticker/C:" +
+        baseCurrency +
+        toCurrency +
+        "/range/1/day/" +
+        FromDate +
+        "/" +
+        ToDate +
+        "?unadjusted=false&sort=asc&limit=32&apiKey=" +
+        apiKey;
+      /* Make the AJAX call */
+      let msg1Object = await fetch(myURL);
+      /* Check the status */
+      if (msg1Object.status >= 200 && msg1Object.status <= 299) {
+        let msg1JSONText = await msg1Object.text();
+        // Parse the JSON string into an object
+        let msg1 = JSON.parse(msg1JSONText);
+        /* Your code to process the result goes here -
+                 display the returned message */
+        let currencyDate = [];
+        let currencyValue = [];
+        let numdays = msg1.results.length;
+        if (numdays > 0) {
+          for (let i = 0; i < numdays; i++) {
+            /* currency close value */
+            currencyValue[i] = msg1.results[i].c;
+            /* date is in Unix milleseconds - create a temporary date letiable */
+            let tempdate = new Date(msg1.results[i].t);
+            /* extract the date string from the value */
+            currencyDate[i] = tempdate.toLocaleDateString();
+          }
+        }
+         let ctx0 = document.getElementById("chartjs-0");
+        var myChart = new Chart(ctx0, {
+          type: "line",
+          data: {
+            labels: currencyDate,
+            datasets: [
+              {
+                label: "1 " + baseCurrency + " to " + toCurrency,
+                data: currencyValue,
+                fill: false,
+                borderColor: "rgb(190, 86, 131)",
+                lineTension: 0.1,
+              },
+            ],
+          },
+          options: {
+            responsive: true,
+            maintainAspectRatio: true,
+            title: {
+              display: true,
+              text: baseCurrency + " to " + toCurrency,
+            },
+          },
+        });
+      } else {
+        /* AJAX completed with error - probably invalid stock ticker symbol */
+        alert("Currency Not Found - Status: " + msg1Object.status);
+        return;
+      }
+    }
+  }
+
+   function clearForm() {
+    document.getElementById("baseCurrency").value = "";
+    document.getElementById("baseCurrencyError").innerText="";
+    document.getElementById("toCurrency").innerText = "";
+    document.getElementById("FromDate").innerText = "";
+    document.getElementById("ToDate").innerText = "";
+     /* Ugly Code to Erase Canvas */
+    var canvas0 = document.getElementById("chartjs-0");
+    var context0 = canvas0.getContext("2d");
+    context0.clearRect(0, 0, canvas0.width, canvas0.height);
+  }
+ 
